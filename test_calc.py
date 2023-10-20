69a0d106bae4207525022afd9cc12bfa7935a3b3
Jakub Grabarczyk <77164148+GraKuba@users.noreply.github.com>
2023-10-20 12:17:31 +0200

    Tests added for all the functions from calc.py

--- test_calc.py
+++ test_calc.py
@@ -1,13 +1,29 @@
 import unittest
-import functions
+import calc
 
 
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = functions.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-5, -10), -15)
+
+    def test_subtract(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-5, -10), -15)
+
+    def test_multiply(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-5, -10), -15)
+
+    def test_divide(self):
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-5, -10), -15)
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
