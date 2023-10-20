9ea47b09693fb8d637ff42730f1fc6f0af4a7db7
Jakub Grabarczyk <77164148+GraKuba@users.noreply.github.com>
2023-10-20 13:25:17 +0200

    setUp and tearDown methods added

--- test_employee.py
+++ test_employee.py
@@ -4,38 +4,39 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    def setUp(self):
+        self.emp_1 = Employee('Cody', 'Schafer', 50000)
+        self.emp_2 = Employee('Sue', 'Smith', 60000)
+
+    def tearDown(self):
+        pass
+
     def test_email(self):
-        emp_1 = Employee('Cody', 'Schafer', 50000)
-        emp_2 = Employee('Sue', 'Smith', 60000)
+        self.assertEqual(self.emp_1.email, 'Cody.Schafer@gmail.com')
+        self.assertEqual(self.emp_2.email, 'Sue.Smith@gmail.com')
 
-        self.assertEqual(emp_1.email, 'Cody.Schafer@gmail.com')
-        self.assertEqual(emp_2.email, 'Sue.Smith@gmail.com')
+        self.emp_1.first = 'John'
+        self.emp_2.first = 'Jane'
 
-        emp_1.first = 'John'
-        emp_2.first = 'Jane'
-
-        self.assertEqual(emp_1.email, 'John.Schafer@gmail.com')
-        self.assertEqual(emp_1.email, 'Jane.Smith@gmail.com')
+        self.assertEqual(self.emp_1.email, 'John.Schafer@gmail.com')
+        self.assertEqual(self.emp_2.email, 'Jane.Smith@gmail.com')
 
     def test_fullname(self):
         emp_1 = Employee('Cody', 'Schafer', 50000)
         emp_2 = Employee('Sue', 'Smith', 60000)
 
         self.assertEqual(emp_1.fullname, 'Cody Schafer')
-        self.assertEqual(emp_1.fullname, 'Sue Smith')
+        self.assertEqual(emp_2.fullname, 'Sue Smith')
 
         emp_1.first = 'John'
         emp_2.first = 'Jane'
 
         self.assertEqual(emp_1.fullname, 'John Schafer')
-        self.assertEqual(emp_1.fullname, 'Jane Smith')
+        self.assertEqual(emp_2.fullname, 'Jane Smith')
 
     def test_apply_raise(self):
-        emp_1 = Employee('Cody', 'Schafer', 50000)
-        emp_2 = Employee('Sue', 'Smith', 60000)
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
 
-        emp_1.apply_raise()
-        emp_2.apply_raise()
-
-        self.assertEqual(emp_1.pay, 52500)
-        self.assertEqual(emp_2.pay, 63000)
+        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_2.pay, 63000)
