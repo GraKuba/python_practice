f144e95ff6d9b86fc2a8abde29340dd933f7ebc3
Jakub Grabarczyk <77164148+GraKuba@users.noreply.github.com>
2023-10-20 11:16:49 +0200

    Main functions for adding, subtracting, multiplying, and dividing created

---
+++ unit testing /main.py
@@ -0,0 +1,21 @@
+
+def add(x, y):
+    # add function
+    return x + y
+
+
+def subtract(x, y):
+    # subtract function
+    return x - y
+
+
+def multiply(x, y):
+    # multiply function
+    return x * y
+
+
+def divide(x, y):
+    # divide function
+    if y == 0:
+        raise ValueError('You cannot divide by 0!')
+    return x / y
